63a19dae0cb6fd9311cfb93547c86908ddae04f4
manuel130489 <74316133+manuel130489@users.noreply.github.com>
2022-09-22 17:13:34 -0500

    Initial commit

---
+++ data/yacimientolaslajas_punto_8.js
@@ -0,0 +1,1 @@
+var json_yacimientolaslajas_punto_8 = {"type":"FeatureCollection","name":"yacimientolaslajas_punto_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"ID":0.0,"nombre":"Yacimiento Las Lajas"},"geometry":{"type":"Point","coordinates":[-69.88823258868274,-47.773268890773572]}}]}
